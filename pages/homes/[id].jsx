05fa176cb661b6983aec1efb118813fbf16b1702
kaywan mohebjalali <mohebjalali5282@gmail.com>
2024-03-17 13:58:37 +0330

    complate page and components [id].jsx

--- pages/homes/[id].jsx
+++ pages/homes/[id].jsx
@@ -1,97 +1,83 @@
-
-
-import { useRouter } from 'next/router'
-import React from 'react'
-import db from '../../data/db.json'
+import { useRouter } from "next/router";
+import React from "react";
+import db from "../../data/db.json";
 
 const Home = () => {
-    const {query:{id}}=useRouter();
-    const {homes}=db;
-    const {title, location, img, roomCount, meterage, price}=homes.find(home=>home.id==id)
-    
-    
+  const { query } = useRouter();
+  const homeId = query?.id;
+  const { homes } = db;
+  const {
+    title,
+    img,
+    roomCount,
+    meterage,
+    price,
+    code,
+    address,
+    buildingType,
+    YearOfConstruction,
+    description,
+  } = homes.find((home) => home.id == homeId);
+
   return (
-    <div className="home-details">
-    <div className="home-details-top">
-        <div className="home-img">
-            <img src={img} alt="" />
+    <div className="container mt-[47vh] sm:mt-[15vh]">
+      <div className="flex justify-evenly gap-4 flex-col lg:flex-row items-center">
+        <div className="home-img  w-[80%] lg:w-[40%]  h-full">
+          <img src={img} alt="" className="w-full h-full" />
         </div>
-        <div className="home-interduce">
-            <div className="home-title">
-                <h1>
-                    <span>فروشگاه مجازی ماکسیما</span>
-                    <span>{price} تومان</span>
-                </h1>
-                <div className="tags">
-                    <span className="tag green-tag">ویژه</span>
-                    <span className="tag brown-tag">برای اجاره</span>
-                </div>
-                <div className="adrress">آدرس : شیراز، میدان ارم</div>
-            </div>
-            <div className="home-review">
-                <div className="home-review-top">
-                    <h2>مرور کلی</h2>
-                    <p className="">
-                        <span>کد ملک : </span>
-                        <span>hz-HZ15</span>
-                    </p>
-                </div>
-                <ul className="home-review-bottom">
-                    <li>
-                        <span>نوع ملک: </span>
-                        <span>مغازه</span>
-                    </li>
-                    <li>
-                        <span>اتاق: </span>
-                        <span>1</span>
-                    </li>
-                    <li>
-                        <span>متراژ</span>
-                        <span>2360</span>
-                    </li>
-                    <li>
-                        <span>سال ساخت</span>
-                        <span>1402</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div className="home-details-bottom">
-        <div className="home-details-description">
-            <p className="">توضیحات</p>
-            <p className="">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
-                صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها
-                و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که
-                لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
-                کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
-                باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و
-                آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با
-                نرم افزارها شناخت بیشتری را برای طراحان رایانه ای
-                علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی
-                ایجاد کرد. در این صورت می توان امید داشت که تمام و
-                دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به
-                پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای
-                اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی
-                اساسا مورد استفاده قرار گیرد. مشاوران دوطرفه به
-                خریدار و فروشنده مشاوره می‌دهند. یک مشاور املاک
-                وظیفه دارد که با صداقت به هر دو طرف معامله مشاوره و
-                راهنمایی درستی بدهد. مشاوران دوطرفه باید از هر دو
-                طرف چه فروشنده و چه خریدار بخواهند که یک قرارداد
-                نمایندگی دوطرفه را امضا کنند. قوانین و مقررات خاصی
-                برای مشاوران دوطرفه وجود دارد، این دست از مشاوران
-                نسبت به دو دسته قبل کارشان سخت‌تر و پیچیده‌تر
-                است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
-                صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها
-                و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که
-                لازم است.
-            </p>
-        </div>
-    </div>
-</div>
 
-  )
-}
+        <div className="w-[80%] lg:w-[60%]">
+          <div className="">
+            <h1 className="flex justify-between font-bold text-[1.3rem] sm:text-2xl">
+              <span className="">{title}</span>
+              <span>{price.toLocaleString()} تومان</span>
+            </h1>
+            <div className=" flex gap-4 my-4">
+              <span className=" green-tag button w-16 bg-green-500">ویژه</span>
+              <span className="brown-tag button  w-16">برای اجاره</span>
+            </div>
+            <div className=" my-4 text-gray-700 text-xl ">{address}</div>
+          </div>
+          <div className="">
+            <div className="flex justify-between items-center mt-8">
+              <h2 className="text-xl sm:text-3xl">مرور کلی</h2>
+              <p className="text-gray-700 sm:text-xl ">
+                <span>کد ملک : </span>
+                <span>{code}</span>
+              </p>
+            </div>
+            <hr className="my-6 " />
+            <ul className="flex flex-col sm:flex-row justify-between gap-4 text-gray-700 text-xl ">
+              <li>
+                <span>نوع ملک : </span>
+                <span>{buildingType}</span>
+              </li>
+              <li>
+                <span>اتاق : </span>
+                <span>{roomCount}</span>
+              </li>
+              <li>
+                <span>متراژ : </span>
+                <span>{meterage}</span>
+              </li>
+              <li>
+                <span>سال ساخت : </span>
+                <span>{YearOfConstruction}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
-export default Home
+      <div className=" mt-8">
+        <div className="">
+          <p className="text-gray-700 text-xl ">توضیحات</p>
+          <hr className="my-4" />
+          <p className="">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
