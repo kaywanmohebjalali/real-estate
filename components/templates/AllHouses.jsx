f12eaa49baa42b102b222a6c56a3bf8673420869
kaywan mohebjalali <mohebjalali5282@gmail.com>
2024-03-19 00:32:55 +0330

    write best code sort in component allHouses

--- components/templates/AllHouses.jsx
+++ components/templates/AllHouses.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Pagination from "../modules/Pagination";
 import db from "../../data/db.json";
 import SearchFilter from "../modules/SearchFilter";
@@ -6,34 +6,55 @@ import Sort from "../modules/Sort";
 import { useRouter } from "next/router";
 
 const AllHouses = () => {
-  const {query}=useRouter()
-  // const [value, setValue]=useState('all')
-  const {homes}=db
-  let [filterDB, finalDB]=''
-  let [keySort, keyHome]=''
-   keySort=query.keySort;
-   keyHome=query.kayHome;
-  filterDB=keyHome?homes.filter(home=>keyHome?.includes(home.title)):homes
-  finalDB =keySort=='room'? (filterDB.sort((p1, p2) =>  p2.roomCount - p1.roomCount )):
-  finalDB =keySort=='price'? (filterDB.sort((p1, p2) =>  p2.price - p1.price )):
-  finalDB =keySort=='size'? (filterDB.sort((p1, p2) =>  p2.meterage - p1.meterage )):filterDB
- 
+  const [render, setRender] = useState("");
+  const { query } = useRouter();
+  const { homes } = db;
+  const filterDB = useRef(homes);
+  const finalDB = useRef(homes);
+  const keySort = useRef("");
+  const keyHome = useRef("");
+
+  keySort.current = query.keySort;
+  keyHome.current = query.kayHome;
+
+  useEffect(() => {
+    filterDB.current = keyHome.current
+      ? homes.filter((home) => keyHome.current?.includes(home.title))
+      : homes;
+
+    finalDB.current =
+      keySort.current == "room"
+        ? filterDB.current.sort((p1, p2) => p2.roomCount - p1.roomCount)
+        : (finalDB.current =
+            keySort.current == "price"
+              ? filterDB.current.sort((p1, p2) => p2.price - p1.price)
+              : (finalDB.current =
+                  keySort.current == "size"
+                    ? filterDB.current.sort(
+                        (p1, p2) => p2.meterage - p1.meterage
+                      )
+                    : filterDB.current));
+    setRender(Math.random());
+    console.log("keySort.current==room :", keySort.current == "room");
+  }, [keySort.current, keyHome.current]);
+
   return (
     <div className="mt-[45vh] sm:mt-[15vh]">
       <div className="container flex justify-between items-center gap-20">
-      <Sort filterField='keySort'/> 
-      <SearchFilter filterField='kayHome'/>
+        <Sort filterField="keySort" />
+        <SearchFilter filterField="kayHome" />
       </div>
-      {!filterDB.length?<div className="w-full text-center text-2xl text-indigo-500 mt-10">خانه ای یافت نشد</div>:<>
-
-      <Pagination key={Math.random()} homes={finalDB} count={4} />
-      </>
-        
-    }
-      </div>
+      {!filterDB.current?.length ? (
+        <div className="w-full text-center text-2xl text-indigo-500 mt-10">
+          خانه ای یافت نشد
+        </div>
+      ) : (
+        <>
+          <Pagination key={Math.random()} homes={finalDB.current} count={4} />
+        </>
+      )}
+    </div>
   );
 };
 
 export default AllHouses;
-
-
